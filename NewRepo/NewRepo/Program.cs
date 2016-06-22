e34ffdeb45ab58ffc9b85ef4531cf7e2f6daa595
Tomas Kravec <tomas.kravec@gmail.com>
2016-06-22 19:20:54 +0200

    som hlupa

---
+++ NewRepo/NewRepo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            asdffds
+                fdas
+
+                fdasfasd
+                    sadf
+                    fds
+        }
+    }
+}
