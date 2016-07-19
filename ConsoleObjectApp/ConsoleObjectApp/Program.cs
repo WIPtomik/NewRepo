a609946085de289931bd97776361d5ea599780e4
Tomas Kravec <tomas.kravec@gmail.com>
2016-07-19 06:04:36 +0200

    keci

---
+++ ConsoleObjectApp/ConsoleObjectApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleObjectApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //add comment
+            HelloWorld hello;
+            hello = new HelloWorld();
+            hello.zdravic();
+            hello.pozdrav("Tomas");
+            Console.ReadKey();
+            //keci keci keci
+            
+        }
+    }
+}
